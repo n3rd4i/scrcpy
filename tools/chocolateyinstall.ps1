6dbbe01069f85f275e5c1c81fa798c7538fbf79e
Andrei Hutanu <andrei.hutanu@evalan.com>
2023-12-10 14:48:11 +0100

    hotfix sw folder intro

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -2,14 +2,33 @@
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 . "$(Join-Path $toolsDir commonEnv.ps1)"
 
+$url = 'https://github.com/Genymobile/scrcpy/releases/download/v2.2/scrcpy-win64-v2.2.zip'
+$folderName = $(Split-Path $url -leaf).replace('.zip', '')
+
+# 1. Download bundle
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
-  unzipLocation = $toolsDir
-  url           = 'https://github.com/Genymobile/scrcpy/releases/download/v2.2/scrcpy-win64-v2.2.zip'
+  fileFullPath  = "$(Join-Path $ENV:TEMP 'scrcpy.zip')"
+  url           = $url
   checksum      = '9F9DA88AC4C8319DCB9BF852F2D9BBA942BAC663413383419CDDF64EAA5685BD'
   checksumType  = 'sha256'
 }
-Install-ChocolateyZipPackage @packageArgs
+$archivePath = Get-ChocolateyWebFile @packageArgs
+
+# 2. Extract bundle in $ENV:TEMP
+$packageArgs = @{
+  packageName     = $env:ChocolateyPackageName
+  unzipLocation   = $ENV:TEMP
+  fileFullPath    = $archivePath
+}
+$extractedFolder = Get-ChocolateyUnzip @packageArgs
+Remove-Item -Path $archivePath
+
+$sourceFolder = Join-Path -Path $extractedFolder -ChildPath $folderName
+if (Test-Path -Path $installLocation) {
+    Copy-Item -Path "$sourceFolder\*" -Destination $installLocation -Recurse -Force
+    Remove-Item -Path $sourceFolder -Recurse -Force
+}
 
 $pp = Get-PackageParameters
 if ($pp['ExcludeADB'] -eq 'true') {
