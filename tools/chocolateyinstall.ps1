6d254725fcfa0a46a6d8123e73ca36d158d2b8f0
Andrei Hutanu <andrei.hutanu@evalan.com>
2025-04-16 08:48:42 +0200

    3.2

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -2,7 +2,7 @@
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 . "$(Join-Path $toolsDir commonEnv.ps1)"
 
-$url = 'https://github.com/Genymobile/scrcpy/releases/download/v3.1/scrcpy-win64-v3.1.zip'
+$url = 'https://github.com/Genymobile/scrcpy/releases/download/v3.2/scrcpy-win64-v3.2.zip'
 $folderName = $(Split-Path $url -leaf).replace('.zip', '')
 
 # 1. Download bundle
@@ -10,7 +10,7 @@ $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   fileFullPath  = "$(Join-Path $ENV:TEMP 'scrcpy.zip')"
   url           = $url
-  checksum      = '0C05EA395D95CFE36BEE974EEB435A3DB87EA5594FF738370D5DC3068A9538CA'
+  checksum      = 'EAA27133E0520979873BA57AD651560A4CC2618373BD05450B23A84D32BEAFD0'
   checksumType  = 'sha256'
 }
 $archivePath = Get-ChocolateyWebFile @packageArgs
